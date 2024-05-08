e5cdf4a0fb86a6c440742783379436dc4c674031
Alliphy <ruebaker29@gmail.com>
2024-05-08 16:03:40 -0600

    finished project, will try black diamond later

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// created a function in order to mark off a checkbox that we have access to through css
+
+// this function also needs to be above our addToDo function in order for the items inside that function to read the nested eventListeners and such
+
+function completeToDo(event) {
+
+    // setting a value within js for the css attribute
+    const value = event.target.getAttribute("aria-checked");
+
+    // using if else statement to target that attribute and set true false to check the box
+    if (value !== "true") {
+        event.target.setAttribute("aria-checked", "true");
+    } else {
+        event.target.setAttribute("aria-checked", "false")
+    }
+}
 // create a function  to remove item from list. we are using event.target.parentNode in order to access the button within our addToDo function
 
 function removeToDo(event) {
@@ -12,6 +28,8 @@ function addToDo(event) {
     // creating a list and list elements for when you click submit
     const item = document.createElement("li");
     item.innerText = document.querySelector("#item").value;
+
+    item.addEventListener("click", completeToDo);
     
     // creating a new button from within this function
     const button = document.createElement("button");
